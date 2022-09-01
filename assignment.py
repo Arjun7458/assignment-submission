fe15ab9a54964c384fd482e30c532f89f80261e5
Arjun <92356897+Arjun7458@users.noreply.github.com>
2022-09-01 21:51:05 +0530

    Day 3 Assignment - Letsupgrade bootcamp course
    Q: Using for loop please print all the prime numbers between 1-200 using FOR LOOP AND RANGE function

---
+++ assignment.py
@@ -0,0 +1,13 @@
+start = 1
+end = 1000
+
+print("Prime numbers between", start, "and", end, "are:")
+
+for num in range(start, end + 1):
+   
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
